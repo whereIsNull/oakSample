2810263318a224441234918df2b258fa678f845d
Cristóbal <cristobalcsc@gmail.com>
2022-04-13 20:42:58 +0200

    test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -17,4 +17,28 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void pruebaReduce() {
+        maxTrailing(Stream.of(7, 2, 3, 10, 2, 4, 8, 1).collect(Collectors.toList()));
+    }
+
+    public static int maxTrailing(List<Integer> arr) {
+        List<Integer> seekedList = new ArrayList<>();
+        seekedList.add(arr.get(0));
+        return arr.subList(1, arr.size()).stream().reduce(-1, (r, e) -> {
+            System.out.println("result: " + r + ", element: " + e);
+            Optional<Integer> currentDiff = seekedList.stream().reduce((maxDiff, current) -> {
+                System.out.println("e - current: " + e + "-" + current + "=" + (e -current));
+                return (((e-current) > maxDiff)? (e-current): maxDiff);
+            });
+            seekedList.add(e);
+            //System.out.println(currentDiff.get());
+            if(r > currentDiff.get()) {
+                return r;
+            } else {
+                return currentDiff.get();
+            }
+        });
+    }
 }
